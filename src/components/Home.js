602be9d98d8234fef3dc43790d0f39fe8349d1ec
Dennis Njau <njaudennis30@gmail.com>
2024-05-08 17:56:48 +0300

    modify home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import NavBar from "./NavBar";
+// import images from 'src/components/backgroundimage.jpg'
+
+
 const Home = () => {
+
 
   return (
     <>
         <header>
         <NavBar />
         </header>
+        <section className='hero'>
         <div className="home">
-        <h2>Welcome to our Job Listing App!</h2>
+          <center>
+        <h1>Welcome to our Job Listing App!</h1>
         <p>Find talented individuals or discover job opportunities.</p>
+        </center>
         </div>
+        </section>
     </>
     
   );
